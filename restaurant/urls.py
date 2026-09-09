d5824d2f42e3f8c63a45c5a63c16f78925ac6dc3
David Kishmishyan <davidkishmishyan@gmail.com>
2026-09-10 01:40:55 +0400

    Finished tasks

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
-
+from .views import MenuItemsView, SingleMenuItemView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', MenuItemsView.as_view()),
+    path('<int:pk>', SingleMenuItemView.as_view()),
 ]
